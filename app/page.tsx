d4fe494c966116e72c9eb266d65a81deff39cdb2
CloudLun <cloud830902@gmail.com>
2023-10-15 22:49:28 -0400

    add sidepanel and mapping

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import Map from './Map'
-import Selection from './Selection';
+import Map from '../components/Map/Map'
+import Landing from '@/components/Landing';
+import SidePanel from '@/components/SidePanel/SidePanel';
 
-import { MapProvider } from "./Context/MapContext";
+import { MapProvider } from "../context/MapContext";
 
 export default function Home() {
   return (
-    <div className='relative'>
+    <div className='relative w-[100vw] h-[100vh] font-sans'>
       <MapProvider >
-        <Selection />
+        <Landing />
+        <SidePanel />
         <Map />
       </MapProvider>
     </div>
