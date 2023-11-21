69e93bfc8ace9976322a54f646380d84b46d40e7
RetroAkabane <ebrohwn19@gmail.com>
2023-11-21 17:11:08 -0500

    fixed yml

--- scripts/legislation_info_generator.py
+++ scripts/legislation_info_generator.py
@@ -5,5 +5,5 @@ api = pyairtable.Api('patt3j0eKCfgvGLAl.b6233ad81bd1d9e1655704868e6fd8a6736d3f9d
 table = api.table('apps7I6q0g9Hyb6j9','tblydWhHOZeqjzycO')
 legislation = table.all()
 legislation_list = [x['fields'] for x in legislation]
-with open(r"..\public\legislation_info.json", "w") as outfile: 
+with open(r"..\public\legislations_info.json", "w") as outfile: 
     json.dump(legislation_list, fp = outfile,indent = 4)
