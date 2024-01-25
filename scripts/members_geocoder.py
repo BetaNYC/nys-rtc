c3a1f5f1216a3cb65004d9bd3705826967993775
Erik Brown <57164699+Erik-Brown01@users.noreply.github.com>
2024-01-25 10:52:02 -0500

    Update members_geocoder.py

--- scripts/members_geocoder.py
+++ scripts/members_geocoder.py
@@ -196,7 +196,7 @@ def generate_members_info(AIRTABLE_API_KEY, AIRTABLE_APP_KEY, AIRTABLE_TBL_KEY, 
 
             if not cached_data.empty:
                 # If the address is in the cache, use the cached latitude and longitude
-                if cached_data.iloc[0]['lat'] is not None and cached_data.iloc[0]['lon'] is not None:
+                if cached_data.iloc[0]['lat'] != np.nan and cached_data.iloc[0]['lon'] != np.nan:
                     member['lat'] = cached_data.iloc[0]['lat']
                     member['lon'] = cached_data.iloc[0]['lon']  # Assuming 'long' is the column name
                     member['Senate_District'] = cached_data.iloc[0]['Senate_District']
